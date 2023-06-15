5ecfa105e3d76aa95eaa717dd5372041b6781414
hoangtd21 <tranduchoang21399@gmail.com>
2023-06-15 16:06:14 +0700

    Update UI

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
-
+  const changeTitleColor = count === 5 ? 'blue' : '';
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1 style={{ color: `${changeTitleColor}` }}>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Count is {count}
         </button>
       </div>
     </>
